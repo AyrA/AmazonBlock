1d7ee2ae6601bd38e654d64b98c4e7563fa5a886
Kevin Gut <kevin.gut@bitm.sg>
2017-11-18 08:10:01 +0100

    Simple Blocker + Cache Handler

---
+++ AmazonBlock/Firewall.cs
@@ -0,0 +1,118 @@
+﻿using NetFwTypeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AmazonBlock
+{
+    public static class Firewall
+    {
+        /// <summary>
+        /// Firewall Rule Direction
+        /// </summary>
+        /// <remarks>
+        /// For TCP, the direction is only applied for the Connection Initiation.
+        /// Blocking Inbound still allows you to connect to any system and receive
+        /// a Response but it prevents them from starting a Connection
+        /// </remarks>
+        [Flags]
+        public enum Direction
+        {
+            /// <summary>
+            /// Inbound
+            /// </summary>
+            In = 1,
+            /// <summary>
+            /// Outbound
+            /// </summary>
+            Out = 2,
+            /// <summary>
+            /// Both Directions
+            /// </summary>
+            Both = In | Out
+        }
+
+        /// <summary>
+        /// Rule Name Prefix
+        /// </summary>
+        public const string BLOCK = "AmazonBlock";
+        public const string COUNTRY = "AMAZON";
+
+        /// <summary>
+        /// Gets the Firewall Policy Object
+        /// </summary>
+        /// <returns></returns>
+        private static INetFwPolicy2 GetPolicy()
+        {
+            return (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+        }
+
+        /// <summary>
+        /// Unblocks A Country
+        /// </summary>
+        /// <param name="CountryCode">Country</param>
+        /// <param name="D">Direction to Unblock</param>
+        public static void UnblockAmazon(AmazonJson Addr, Direction D = Direction.Both)
+        {
+            var Policy = GetPolicy();
+            if (D == Direction.Both)
+            {
+                UnblockAmazon(Addr, Direction.In);
+                UnblockAmazon(Addr, Direction.Out);
+            }
+            else
+            {
+                var Key = $"{BLOCK}-{D}-{COUNTRY}";
+                var Rules = Policy.Rules.Cast<INetFwRule2>().Select(m => m.Name).ToArray();
+                for (var i = 0; i < Rules.Count(m => m == Key); i++)
+                {
+                    Policy.Rules.Remove(Key);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Blocks A Country
+        /// </summary>
+        /// <param name="CountryCode">Country Code</param>
+        /// <param name="IPList">IP Address List to Block</param>
+        /// <param name="D">Direction to Block</param>
+        /// <remarks>This first completely unblocks said Country</remarks>
+        public static void BlockAmazon(AmazonJson Addr, Direction D = Direction.Both)
+        {
+            if (D == Direction.Both)
+            {
+                BlockAmazon(Addr, Direction.In);
+                BlockAmazon(Addr, Direction.Out);
+            }
+            else
+            {
+                UnblockAmazon(Addr, D);
+                IpBlock(Addr.prefixes.Select(m => m.ip_prefix).ToArray(), D);
+                IpBlock(Addr.ipv6_prefixes.Select(m => m.ipv6_prefix).ToArray(), D);
+            }
+        }
+
+        private static void IpBlock(string[] IPList, Direction D)
+        {
+            var Policy = GetPolicy();
+            for (var I = 0; I < IPList.Length; I += 1000)
+            {
+                Console.Error.Write('.');
+                var R = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwRule"));
+
+                R.Name = $"{BLOCK}-{D}-{COUNTRY}";
+                R.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                R.Description = $"Blocking All IP Addresses of {COUNTRY}";
+                R.Direction = D == Direction.In ? NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN : NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+                R.Enabled = true;
+                R.InterfaceTypes = "All";
+                R.LocalAddresses = "*";
+                R.Profiles = int.MaxValue;
+                R.Protocol = 256;
+                R.RemoteAddresses = string.Join(",", IPList.Skip(I).Take(1000).ToArray());
+                Policy.Rules.Add(R);
+            }
+        }
+    }
+}
--- AmazonBlock/Program.cs
+++ AmazonBlock/Program.cs
@@ -1,15 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace AmazonBlock
 {
     class Program
     {
+        public const string ADDR = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+
+        private static string _CacheFile;
+
+        public static string CacheFile
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_CacheFile))
+                {
+                    using (var P = Process.GetCurrentProcess())
+                    {
+                        Path.Combine(Path.GetDirectoryName(P.MainModule.FileName), "cache.json");
+                    }
+                }
+                return _CacheFile;
+            }
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length > 0 && args.Contains("/?"))
+            {
+                Help();
+            }
+            else
+            {
+                AmazonJson JSON;
+                if (!File.Exists(CacheFile) || DateTime.UtcNow.Subtract(File.GetLastWriteTimeUtc(CacheFile)).TotalDays >= 1.0)
+                {
+                    Console.Error.WriteLine("Downloading Addresses");
+                    using (var WC = new WebClient())
+                    {
+                        try
+                        {
+                            JSON = JsonConvert.DeserializeObject<AmazonJson>(WC.DownloadString(ADDR));
+                            File.WriteAllText(CacheFile, JsonConvert.SerializeObject(JSON, Formatting.Indented));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Error.WriteLine("Unable to download JSON: {0}", ex.Message);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("Taking JSON from Cache");
+                    JSON = JsonConvert.DeserializeObject<AmazonJson>(File.ReadAllText(CacheFile));
+                }
+                Console.Error.WriteLine("Processing Addresses");
+                Firewall.BlockAmazon(JSON);
+            }
+#if DEBUG
+            Console.Error.WriteLine("#END");
+            Console.ReadKey(true);
+#endif
+        }
+
+        private static void Help()
+        {
+            Console.Error.WriteLine(@"BlockAmazon.exe /i /o
+Blocks Amazons Network
+
+/i  - Block inbound. Recommended for service providers that want to block
+      most automated requests.
+/o  - Block outbound. Recommended if users can submit an address to connect
+      to and the service provider wants to avoid Amazons network.
+
+To block both directions, /i and /o must be used simultaneously. Any
+direction not specified will be unblocked, therefore running this
+application without any arguments unblocks the entire Network.
+The cache file is kept for 24 hours.
+
+");
         }
     }
 }
